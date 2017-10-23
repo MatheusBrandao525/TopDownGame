02b3ca690c0ca8b7b29a1308964bf3732512c8cd
Michael Cline <mcline@gohcl.com>
2017-10-23 16:58:49 -0400

    Initial node server setup and dependencies

---
+++ app.js
@@ -0,0 +1,21 @@
+var express    = require('express'),
+    app        = express(),
+    bodyParser = require("body-parser");
+
+
+    app.use(express.static(__dirname + "/public")); // set default folder to /public
+
+    app.use(bodyParser.urlencoded({extended: true})); //body parser for form data
+    app.set("view engine", "ejs");                  //  Using EJS templates
+  //  app.use(methodOverride("_method"));
+  //  app.use(flash());
+
+
+  app.get("/", function (req, res) {
+      res.render("index");
+  });
+
+  app.listen("8080", "127.0.0.1", function () {
+     console.log("The Server has Started!!");
+
+  });
